6981569e909fceb0424bef3372234ccb502c463e
Szerencsés-Fekete Dániel <dnlmac@szerencses-macbook.local>
2025-05-21 15:20:35 +0200

    Bugfix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,8 +19,22 @@ export default function RootLayout({
     <html lang="de" className="scroll-smooth">
       <head>
         <link rel="icon" href="/favicon.jpg" type="image/jpeg" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17078731522"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17078731522');
+          `}
+        </Script>
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
